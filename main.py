2d8c82b81d8071ff4a408050495af2aa924778ac
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-05-27 11:08:47 +0200

    add: get_ip_info in main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_ip_info(ip):
+    try:
+        response = requests.get(f"http://ip-api.com/json/{ip}").json()
+        return {
+            "country": response.get("country", "N/A"),
+            "city": response.get("city", "N/A"),
+            "isp": response.get("isp", "N/A")
+        }
+    except:
+        return {"country": "N/A", "city": "N/A", "isp": "N/A"}
