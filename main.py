e9255972d1fbe2183148442313694d3a0627813a
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-05-28 18:45:32 +0200

    add: menu

--- main.py
+++ main.py
@@ -4,7 +4,26 @@ def main():
     print_welcome_message()
 
     while True:
-        get_ip_info("ip_address")
+
+        print(""" 
+            [1] Amazon
+            [2] Ebay
+            [3] Linkedin
+            [4] Netflix
+        """)
+
+        choice = input("Insert your choice: ")
+
+        if choice == "1":
+            print("you choose Amazon")
+        elif choice == "2":
+            print("you choose Ebay")
+        elif choice == "3":
+            print("you choose Linkedin")
+        elif choice == "4":
+            print("you choose Netflix")
+        else:
+            print("Invalid choice")
 
 if __name__ == "__main__":
     main()
