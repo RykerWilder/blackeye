8be7c97220a3edebc074dc9e8ce08924586d1330
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-05-28 18:50:07 +0200

    remove while loop

--- main.py
+++ main.py
@@ -3,27 +3,25 @@ from utils import print_welcome_message, get_ip_info
 def main():
     print_welcome_message()
 
-    while True:
+    print(""" 
+        [1] Amazon
+        [2] Ebay
+        [3] Linkedin
+        [4] Netflix
+    """)
 
-        print(""" 
-            [1] Amazon
-            [2] Ebay
-            [3] Linkedin
-            [4] Netflix
-        """)
+    choice = input("Insert your choice: ")
 
-        choice = input("Insert your choice: ")
-
-        if choice == "1":
-            print("you choose Amazon")
-        elif choice == "2":
-            print("you choose Ebay")
-        elif choice == "3":
-            print("you choose Linkedin")
-        elif choice == "4":
-            print("you choose Netflix")
-        else:
-            print("Invalid choice")
+    if choice == "1":
+        print("you choose Amazon")
+    elif choice == "2":
+        print("you choose Ebay")
+    elif choice == "3":
+        print("you choose Linkedin")
+    elif choice == "4":
+        print("you choose Netflix")
+    else:
+        print("Invalid choice")
 
 if __name__ == "__main__":
     main()
