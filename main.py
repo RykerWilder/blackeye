0b454b40db6ec7b469e8fc4ad1d5fecddb4df4cb
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-05-28 11:19:09 +0200

    move get_ip_info in utils.py

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
-from utils import print_logo
-import requests
+from utils import print_logo, get_ip_info
+
 
 print_logo()
-
-def get_ip_info(ip):
-    ip_data = {}
-    try:
-        response = requests.get(f"http://ip-api.com/json/{ip}").json()
-        ip_data = {
-            "country": response.get("country", "N/A"),
-            "country-code": response.get("countryCode", "N/A"),
-            "region": response.get("regionName", "N/A"),
-            "city": response.get("city", "N/A"),
-            "latitude": response.get("lat", "N/A"),
-            "longitude": response.get("lon", "N/A"),
-            "timezone": response.get("timezone", "N/A"),
-            "organization": response.get("org", "N/A"),
-            "as": response.get("as", "N/A"),
-            "isp": response.get("isp", "N/A")
-        }
-        return print(ip_data)
-    except Exception as e:
-        print(f"Error retrieving IP information: {e}")
 
 get_ip_info("ip_address")
